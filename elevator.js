5da07ebad4219a27fe51496b04aa206e830fd46e
James Sullivan <jamesfsullivan5@gmail.com>
2017-08-27 22:16:56 -0600

    Add functions to pass level 2.

--- elevator.js
+++ elevator.js
@@ -1,8 +1,26 @@
 export default class Elevator {
   constructor() {
-    this.floor = 0;
     this.currentFloor = 0;
     this.queue = [];
     this.passengers = [];
+    this.floorCount = 0;
+  }
+
+  goToFloor(person) {
+    while (this.currentFloor !== person.currentFloor) {
+      this.currentFloor > person.currentFloor ? (this.currentFloor--, this.floorCount++) : (this.currentFloor++, this.floorCount++);
+    }
+
+    while (this.currentFloor !== person.dropOffFloor) {
+      this.currentFloor > person.dropOffFloor ? (this.currentFloor--, this.floorCount++) :
+      (this.currentFloor++, this.floorCount++)
+    }
+  }
+
+  reset() {
+    this.currentFloor = 0;
+    this.queue = [];
+    this.passengers = [];
+    this.floorCount = 0;
   }
 }
